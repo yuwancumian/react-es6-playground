742b8a1b8a2dd54d2c95a1d5c226dae760992b06
yuwancumian <yuwancumian@hotmail.com>
2016-02-27 12:27:25 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: './main.js',
+  output: {
+    filename: './dist/bundle.js'       
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
