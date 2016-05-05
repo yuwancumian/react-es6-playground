6bc6a89ab0d0e74632b5fdfd8f45ab1c4ca8b74c
yuwancumian <yuwancumian@hotmail.com>
2016-05-05 10:58:13 +0800

    test antd

--- components/app.js
+++ components/app.js
@@ -1,12 +1,26 @@
-'use strict';
 import React from 'react';
+import { Button } from 'antd';
 
-export default React.createClass({
-
-    render() {
-        return (
-            <h2> hello react </h2>
-        );
-    }
-
+const App = React.createClass({
+  getInitialState() {
+    return {
+      date: ''
+    };
+  },
+  handleChange(value) {
+    message.info('您选择的日期是: ' + value.toString());
+    this.setState({
+      date: value
+    });
+  },
+  render() {
+    return <div>
+        <Button type="primary">主按钮</Button>
+        <Button>次按钮</Button>
+        <Button type="ghost">幽灵按钮</Button>
+        <Button type="dashed">虚线按钮</Button>
+    </div>;
+  }
 });
+
+export default App;
